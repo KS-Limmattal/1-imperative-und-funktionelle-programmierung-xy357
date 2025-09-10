b8a48a46e43fcb57b75dfabd5ed90a54fe25afd6
xy357 <philip.boettiger@bluewin.ch>
2025-09-10 22:27:03 +0200

    1.teil loops d)

--- Loops.java
+++ Loops.java
@@ -1,28 +1,40 @@
 public class Loops {
     static void primeFactorisation(int n){
-        for(int i=2; n > 1;i++) {
-            if (n % i==0){
-                n = n / i;
-                System.out.println(n);
-            } else {
-                //i=i+1;
-                System.out.println(i);
-            }
+        int i=2;
+        if (n%i==0){
+            n=n/i;
+            System.out.println(n);
+        } else {
+            System.out.println(i);
         }
     }
 
     // TODO: Schreibe hier die Methoden für die Teilaufgaben.
-    
+    static double pi(int d){
+        double p=0;
+        double z=0;
+        for (int k=0; k<=2451;){
+            double a=Math.pow(-1,k);
+            double b=(2*k)+1;
+            double c=a/b;
+            p=4*c;
+            z=z+p;
+            k=k+1;
+        
+        }
+        return (z);
+    }
     public static void main(String[] args){
         
         // Test-Code für Teilaufgabe c)
         System.out.println("Aufgabe c)");
-        primeFactorisation(3);
+        primeFactorisation(6);
         // TODO: Schreibe mehr Testcode
         System.out.println();
-
+        
         // Test-Code für Teilaufgabe d)
         System.out.println("Aufgabe d)");
+        System.out.println(pi(0));
         for (int n = 0; n < 12; n++){
             //System.out.println("Pi approximated to " + n + " digits with pi()  is " + pi(n));
         }
